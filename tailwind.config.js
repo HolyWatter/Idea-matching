4ba9444891a318cb0268e91cef9fc5c86902f5ac
HolyWatter <qktjdtn97@icloud.com>
2022-12-26 14:43:50 +0900

    Project setting

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+//경로 
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
