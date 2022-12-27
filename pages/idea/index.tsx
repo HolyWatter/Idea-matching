dd0f06edea00a569037e0f5957739d84188726cf
HolyWatter <qktjdtn97@icloud.com>
2022-12-27 21:57:05 +0900

    작업중 : 아이디어 등록 레이아웃 임시

---
+++ pages/idea/index.tsx
@@ -0,0 +1,11 @@
+export default function Idea() {
+  return (
+    <div className="flex justify-center space-x-10 mt-6">
+      <div className="w-[400px] h-[400px] p-5 border shadow-lg rounded-md">
+        {" "}
+        asd
+      </div>
+      <div className="w-[800px] p-5 border shadow-lg rounded-md"> asd</div>
+    </div>
+  );
+}
