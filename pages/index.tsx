4941d3e673967c9f93dc29f8d562195e974c9cb1
HolyWatter <qktjdtn97@icloud.com>
2023-02-14 21:11:54 +0900

    간단한 메인페이지 레이아웃작성

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
-    <div>
-      <h1>메인페이지</h1>
+    <div className="h-full flex-col flex items-center justify-center space-y-5">
+      <h1 className="text-5xl mt-16">프로젝트 매칭 서비스</h1>
+      <p>아이디어를 등록해서 프로젝트 팀원을 모집해 보세요</p>
     </div>
   );
 }
